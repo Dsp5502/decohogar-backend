707d365236714e5f2db8bf704e804e04b1caaef7
David Puerto Guerrero <dsp5502@gmail.com>
2023-03-01 09:48:34 -0500

    agregando requirements

--- fabfile.py
+++ fabfile.py
@@ -5,3 +5,7 @@ from fabric import task, Connection
 def deploy(c: Connection):
     with c.cd('~/project/decohogar-backend/'):
         c.run('git pull')
+    with c.cd('~/project/'):
+        with c.prefix('source env/bin/activate'):
+            with c.cd('decohogar-backend/'):
+                c.run('pip install -r requirements.txt')
