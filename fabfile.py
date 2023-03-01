d765dce03d2bc7057aad18132d5bea46c8c04078
David Puerto Guerrero <dsp5502@gmail.com>
2023-03-01 10:13:10 -0500

    2

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,8 @@ from fabric import task, Connection
 @task
 def deploy(c: Connection):
     with c.cd('~/project/decohogar-backend/'):
-        with c.prefix('source env/bin/activate'):
-            c.run('git pull')
-            with c.cd('..'):
-                c.run('pip install -r requirements.txt')
+        c.run('git pull')
+        with c.local('cd ..'):
+            with c.prefix('source env/bin/activate'):
+                with c.cd('decohogar-backend/'):
+                    c.run('pip install -r requirements.txt')
