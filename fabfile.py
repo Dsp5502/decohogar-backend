41ec9498c099c2fc77987e2672f0866636f78111
David Puerto Guerrero <dsp5502@gmail.com>
2023-03-01 10:41:00 -0500

    restart

--- fabfile.py
+++ fabfile.py
@@ -11,3 +11,8 @@ def deploy(c: Connection):
 
     with c.cd('~/project/decohogar-backend/'):
         c.run('pip install -r requirements.txt')
+
+    c.sudo('systemctl restart decohogar-backend')
+    c.sudo('systemctl restart nginx')
+
+    print('Deployed!')
