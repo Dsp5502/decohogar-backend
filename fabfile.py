2983f16d9b3238ebcb89d6c8f82c33946d0fb3fb
David Puerto Guerrero <dsp5502@gmail.com>
2023-03-01 10:23:50 -0500

    prueba entorno activado

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,8 @@ from fabric import task, Connection
 
 @task
 def deploy(c: Connection):
-    with c.cd('~/project/decohogar-backend/'):
-        c.run('git pull')
-        with c.local('cd ..'):
-            with c.prefix('source env/bin/activate'):
-                with c.cd('decohogar-backend/'):
-                    c.run('pip install -r requirements.txt')
+    with c.cd('~/project/'):
+        with c.prefix('source env/bin/activate'):
+            with c.cd('decohogar-backend/'):
+                c.run('git pull')
+                c.run('pip install -r requirements.txt')
