acbb7d593b5e87fbc701342c3f448b8db05910fa
David Puerto Guerrero <dsp5502@gmail.com>
2023-03-01 10:09:24 -0500

    cambiando el orden de fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ from fabric import task, Connection
 @task
 def deploy(c: Connection):
     with c.cd('~/project/decohogar-backend/'):
-        c.run('git pull')
-        with c.lcd('..'):
-            with c.prefix('source env/bin/activate'):
+        with c.prefix('source env/bin/activate'):
+            c.run('git pull')
+            with c.cd('..'):
                 c.run('pip install -r requirements.txt')
